88fd7dea61a4f52deb3c8c3cc79086e60c018980
Pasquale <pasqualesansonna@hotmail.it>
2020-04-17 20:23:29 +0200

    SplashActivity

---
+++ app/src/main/java/it/uniba/di/gruppo17/SplashActivity.java
@@ -0,0 +1,29 @@
+package it.uniba.di.gruppo17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * @author Pasquale
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_LAYOUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent  MainActivityIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(MainActivityIntent);
+                finish();
+            }
+        }, SPLASH_TIME_LAYOUT);
+}
+}
