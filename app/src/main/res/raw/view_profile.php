dcee8c998dbb4faa351dc7a178f2a87a4cc9d269
Sgarra <sgarraclaudia0@gmai.com>
2020-04-27 19:26:22 +0200

    Grafica profilo utente

--- app/src/main/res/raw/view_profile.php
+++ app/src/main/res/raw/view_profile.php
@@ -1,14 +1,14 @@
 <?php
 //restituisce un oggetto JSON con nome, cognome, email
-include 'vars.php'
+include 'vars.php';
 
 header ('Content-Type: application/json' );
 
-$id = $_GET ["id"];
+$id = $_GET ["id_utente"];
 
 if ($id != null) {
 	$conn = mysqli_connect ( $host, $user, $password, $db_name );
-	$query = "select nome, cognome, email from utente where id='$id'";
+	$query = "select nome, cognome, email from utenti where id='$id_utente'";
 
 	// Check connection
 	if (mysqli_connect_errno ()) {
@@ -23,9 +23,9 @@ if ($id != null) {
 	$number = mysqli_fetch_array ( $result );
 
 	$post_data = json_encode ( array (
-			'nome' => $number ['nome'] 
-			'cognome' => $number ['cognome'] 
-			'email' => $number ['email'] 
+			'nome' => $number ['nome'],
+			'cognome' => $number ['cognome'],
+			'email' => $number ['email'],
 	), JSON_FORCE_OBJECT );
 	
 	echo $post_data;
