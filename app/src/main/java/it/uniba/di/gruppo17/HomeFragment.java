548293d7697eb837b436468c39b69d3b3e723c71
Pasquale <pasqualesansonna@hotmail.it>
2020-04-29 18:07:41 +0200

    Creazione Home (manca wallet e da modificare tendina, per il resto ENJOY)

--- app/src/main/java/it/uniba/di/gruppo17/HomeFragment.java
+++ app/src/main/java/it/uniba/di/gruppo17/HomeFragment.java
@@ -9,29 +9,53 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 public class HomeFragment extends Fragment {
 
 
-    private Button BT_searchScooter;
+    private ImageView IV_searchScooter;
+    private ImageView IV_profile;
+    private ImageView IV_wallet;
 
+    ImageView image;
+    ImageView image2 = null;
+    LinearLayout linearLayout = null;
+    Animation logo_anim = null;
+    Animation bg_anim;
+    Animation fromBottom;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        BT_searchScooter = (Button) view.findViewById(R.id.buttonSearchScooter);
+        IV_searchScooter = (ImageView) view.findViewById(R.id.IV_SearchScooter);
+        IV_profile = (ImageView) view.findViewById(R.id.IV_profile);
+        IV_wallet = (ImageView) view.findViewById(R.id.IV_wallet);
+        image = (ImageView) view.findViewById(R.id.imageBG);
+        image2 = (ImageView) view.findViewById(R.id.imageLogo);
+        linearLayout = (LinearLayout) view.findViewById(R.id.LinearLayoutButton);
 
+        fromBottom = AnimationUtils.loadAnimation(this.getActivity(), R.anim.frombottom);
+        logo_anim = AnimationUtils.loadAnimation(this.getActivity(), R.anim.logo_scale);
+        bg_anim = AnimationUtils.loadAnimation(this.getActivity(), R.anim.bg_home);
+        fromBottom.setDuration(1000);
+        linearLayout.startAnimation(fromBottom);
+        image.animate().translationY(-1650).setDuration(800);
+        logo_anim.setDuration(1000);
+        image2.startAnimation(logo_anim);
 
-        BT_searchScooter.setOnClickListener(new View.OnClickListener() {
+        IV_searchScooter.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
             public void onClick(View v) {
-
                 FragmentManager fragmentManager = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 Fragment nextFragment = new MapsFragment();
@@ -40,6 +64,38 @@ public class HomeFragment extends Fragment {
 
             }
         });
+
+        IV_profile.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment nextFragment = new ProfileFragment();
+                fragmentTransaction.replace(R.id.fragment_container, nextFragment);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        /** @TODO fragment portafoglio
+/*
+        IV_wallet.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment nextFragment = new WalletFragment();
+                fragmentTransaction.replace(R.id.fragment_container, nextFragment);
+                fragmentTransaction.commit();
+
+            }
+        });
+        */
+
 
         return view;
     }
--- app/src/main/java/it/uniba/di/gruppo17/util/Keys.java
+++ app/src/main/java/it/uniba/di/gruppo17/util/Keys.java
@@ -3,7 +3,7 @@ package it.uniba.di.gruppo17.util;
 public interface Keys {
     String EMAIL = "email";
     String PASSWORD = "password";
-    String SERVER = "http://192.168.1.104/";
+    String SERVER = "http://192.168.178.22/";
     String USER_AGENT = "Mozilla/5.0";
     String ID_UTENTE = "id";
     String SHARED_PREFERENCES = "MovingFastPreferences";
