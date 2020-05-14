16c0b2eff7e83ffe9c5d576786f65fb211a55dbe
franco126 <GitHub-998>
2020-05-14 18:48:45 +0200

    Migliorata gestione controlli nfc e internet in RentFragment Aggiunto fragment per quei dispositivi che non hanno nfc

---
+++ app/src/main/java/it/uniba/di/gruppo17/NoChipNfcFragment.java
@@ -0,0 +1,33 @@
+package it.uniba.di.gruppo17;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ *
+ */
+public class NoChipNfcFragment extends Fragment {
+
+    public NoChipNfcFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_no_chip_nfc, container, false);
+    }
+}
